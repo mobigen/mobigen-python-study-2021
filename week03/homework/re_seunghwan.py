1b524e1be2b814812be06db82515fcf479d3239c
seungfwani <seungfwani2@gmail.com>
2021-07-05 21:30:45 +0900

    update

--- week03/homework/re_seunghwan.py
+++ week03/homework/re_seunghwan.py
@@ -1,7 +1,7 @@
 import re
 
 
-with open('../wiki_python.html', 'r', encoding='UTF-8') as f:
+with open('../bootstrap_example.html', 'r', encoding='UTF-8') as f:
     data = f.read()
 
 # pattern = r'\<a.+href\=\"(.+?)\".+\>(.+)\<\/a\>'
@@ -10,8 +10,10 @@ with open('../wiki_python.html', 'r', encoding='UTF-8') as f:
 # for url, value in matched:
 #     print(f"[{value}] is [{url}]")
 
-pattern = r'\<a.+href\=(?P<dquote>\")(.+?)(?P=dquote).+\>(.+)\<\/a\>'
+pattern = r'\<a.+?href\=(?P<dquote>\")(.+?)(?P=dquote).*?\>([\s\S]+?)\<\/a\>'
 matched = re.findall(pattern, data)
 
 for _, url, value in matched:
     print(f"[{value}] is [{url}]")
+
+print(len(matched))
