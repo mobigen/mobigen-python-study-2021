d68e1376d3015f9a5ff15d7a65076f8df05029fa
hyobin <ihyobin@hyobinui-MacBookAir.local>
2021-07-06 09:30:09 +0900

    homework3

---
+++ week03/homework/word_count_hb.py
@@ -0,0 +1,26 @@
+import re
+
+def word_count(textList):
+    r = dict()
+    for i in textList:
+        if i in r:
+            r[i] += 1
+        else:
+            r[i] = 1
+    return r   
+
+if __name__ == "__main__" :
+    f = open('../wiki_python.txt','r')
+
+    data = f.read().replace('\n', ' ')
+    words = re.findall(r'\w+', data)
+
+    counts = word_count(words)
+    sorted_words = sorted(counts.items(),key=lambda x:x[1],reverse=True)
+    
+    for i in range(10):
+        print(sorted_words[i])
+
+
+
+
